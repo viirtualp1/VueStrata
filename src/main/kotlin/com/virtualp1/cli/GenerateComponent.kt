2bfad5aaa1814e3cb5bfc2bb73f343ae2aef68d7
virtualp1 <zininnikita309@gmail.com>
2025-07-09 15:06:01 +0400

    add initial project configuration and implement code style settings

--- src/main/kotlin/com/virtualp1/cli/GenerateComponent.kt
+++ src/main/kotlin/com/virtualp1/cli/GenerateComponent.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class GenerateComponent : AnAction() {
-    override fun actionPerformed(p0: AnActionEvent) {
-        TODO("Not yet implemented")
-    }
+  override fun actionPerformed(p0: AnActionEvent) {
+    TODO("Not yet implemented")
+  }
 }
--- src/main/kotlin/com/virtualp1/cli/GenerateDomain.kt
+++ src/main/kotlin/com/virtualp1/cli/GenerateDomain.kt
@@ -9,57 +9,57 @@ import com.intellij.psi.PsiManager
 import com.intellij.openapi.command.WriteCommandAction
 
 class GenerateDomain : AnAction() {
-    var layers = arrayOf("entities", "features", "widgets", "page-widgets", "pages", "app")
+  var layers = arrayOf("entities", "features", "widgets", "page-widgets", "pages", "app")
 
-    override fun actionPerformed(p0: AnActionEvent) {
-        val project = p0.project ?: return
-        val virtualFile = p0.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
-        val psiDir = PsiManager.getInstance(project).findDirectory(virtualFile) ?: return
+  override fun actionPerformed(p0: AnActionEvent) {
+    val project = p0.project ?: return
+    val virtualFile = p0.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+    val psiDir = PsiManager.getInstance(project).findDirectory(virtualFile) ?: return
 
-        val name = Messages.showInputDialog(
-            project,
-            "Enter new domain name",
-            "@frontend/cli",
-            AllIcons.Nodes.Folder,
-        )
+    val name = Messages.showInputDialog(
+      project,
+      "Enter new domain name",
+      "@frontend/cli",
+      AllIcons.Nodes.Folder,
+    )
 
-        if (name == null || name.isEmpty()) {
-            Messages.showErrorDialog(
-                project,
-                "Domain name cannot be empty",
-                "@frontend/cli"
-            )
+    if (name == null || name.isEmpty()) {
+      Messages.showErrorDialog(
+        project,
+        "Domain name cannot be empty",
+        "@frontend/cli"
+      )
 
-            return
-        }
-
-        val existingDir = psiDir.findSubdirectory(name)
-        if (existingDir != null) {
-            Messages.showErrorDialog(
-                project,
-                "Directory '$name' already exists",
-                "@frontend/cli"
-            )
-            return
-        }
-
-        WriteCommandAction.runWriteCommandAction(project) {
-            psiDir.createSubdirectory(name)
-            val domainDir = psiDir.findSubdirectory(name)
-
-            for (layer in layers) {
-                val layerDir = domainDir?.findSubdirectory(layer)
-
-                if (layerDir == null) {
-                    domainDir?.createSubdirectory(layer)
-                } else {
-                    Messages.showErrorDialog(
-                        project,
-                        "Layer $layer already created",
-                        "My CLI Plugin"
-                    )
-                }
-            }
-        }
+      return
     }
+
+    val existingDir = psiDir.findSubdirectory(name)
+    if (existingDir != null) {
+      Messages.showErrorDialog(
+        project,
+        "Directory '$name' already exists",
+        "@frontend/cli"
+      )
+      return
+    }
+
+    WriteCommandAction.runWriteCommandAction(project) {
+      psiDir.createSubdirectory(name)
+      val domainDir = psiDir.findSubdirectory(name)
+
+      for (layer in layers) {
+        val layerDir = domainDir?.findSubdirectory(layer)
+
+        if (layerDir == null) {
+          domainDir?.createSubdirectory(layer)
+        } else {
+          Messages.showErrorDialog(
+            project,
+            "Layer $layer already created",
+            "My CLI Plugin"
+          )
+        }
+      }
+    }
+  }
 }
