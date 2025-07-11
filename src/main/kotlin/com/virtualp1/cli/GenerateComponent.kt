bbe1f7e145d3a193681814d2cdcc7bb606bce6f3
virtualp1 <zininnikita309@gmail.com>
2025-07-11 23:11:25 +0400

    implement GenerateComponent action to create new domain directories with initial files

--- src/main/kotlin/com/virtualp1/cli/GenerateComponent.kt
+++ src/main/kotlin/com/virtualp1/cli/GenerateComponent.kt
@@ -1,10 +1,76 @@
 package com.virtualp1.cli
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiManager
+import kotlin.text.isEmpty
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.vfs.VirtualFile
+
+fun VirtualFile.toDocument(): Document? =
+  FileDocumentManager.getInstance().getDocument(this)
 
 class GenerateComponent : AnAction() {
   override fun actionPerformed(p0: AnActionEvent) {
-    TODO("Not yet implemented")
+    val project = p0.project ?: return
+    val virtualFile = p0.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+    val psiDir = PsiManager.getInstance(project).findDirectory(virtualFile) ?: return
+
+    val name = Messages.showInputDialog(
+      project,
+      "Enter new domain name",
+      "@frontend/cli",
+      AllIcons.Nodes.Folder,
+    )
+
+    if (name == null || name.isEmpty()) {
+      Messages.showErrorDialog(
+        project,
+        "Domain name cannot be empty",
+        "@frontend/cli"
+      )
+
+      return
+    }
+
+    val existingDir = psiDir.findSubdirectory(name)
+    if (existingDir != null) {
+      Messages.showErrorDialog(
+        project,
+        "Directory '$name' already exists",
+        "@frontend/cli"
+      )
+      return
+    }
+
+    WriteCommandAction.runWriteCommandAction(project) {
+      val subDir = psiDir.createSubdirectory(name)
+
+      val indexFile = subDir.createFile("index.ts")
+      val vueFile = subDir.createFile("$name.vue")
+      subDir.createFile("$name.scss")
+      subDir.createFile("locales.json")
+
+      val vueContent = """
+        <template></template>
+        
+        <script setup lang="ts"></script>
+        
+        <style lang="scss" src="./$name.scss"></style>
+      """.trimIndent()
+      val vueDocument = vueFile.virtualFile.toDocument()
+      vueDocument?.setText(vueContent)
+
+      val indexContent = """
+        export $name from './$name.vue'
+      """.trimIndent()
+      val indexDocument = indexFile.virtualFile.toDocument()
+      indexDocument?.setText(indexContent)
+    }
   }
 }
